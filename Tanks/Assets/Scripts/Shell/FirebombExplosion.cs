c58f8d1492c1f1f0ca81dc25a8938d769fd7e25d
Calvin Yoh <flashfire340@gmail.com>
2020-02-06 00:18:23 -0800

    Firebomb Part 1
    Created firebomb, as well as lava field that does DOT. Also made it so that the tanks currently alternate between the two different weapons.

---
+++ Tanks/Assets/Scripts/Shell/FirebombExplosion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebombExplosion : MonoBehaviour
+{
+    public LayerMask m_GroundMask;
+    public ParticleSystem m_ExplosionParticles;
+    public AudioSource m_ExplosionAudio;
+    public float m_MaxLifeTime = 2f;
+    public Transform m_Lavafield;
+    //public FireTiles m_fireTiles;
+
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+
+        if (!other.GetComponent<Rigidbody>())
+        {
+
+            Instantiate(m_Lavafield, transform.position, Quaternion.identity);
+            m_ExplosionParticles.transform.parent = null;
+
+            m_ExplosionParticles.Play();
+
+            m_ExplosionAudio.Play();
+
+            Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Tanks/Assets/Scripts/Shell/LavaFieldScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaFieldScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float m_LifeTime = 3f;
+    public LayerMask m_TankMask;
+
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 15f, m_TankMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            Debug.Log("this is a tank");
+
+            if (!targetRigidbody)
+                continue;
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            if (!targetHealth)
+                continue;
+
+            float damage = 0.5f;
+
+            targetHealth.TakeDamage(damage);
+        }
+    }
+
+}
